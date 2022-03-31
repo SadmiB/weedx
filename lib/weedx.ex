acb997df8d7073c50461d6e7783a315a16c60f36
SadmiB <sadmibouhafs@gmail.com>
2022-03-31 12:23:59 +0100

    init commit

---
+++ lib/weedx.ex
@@ -0,0 +1,18 @@
+defmodule Weedx do
+  @moduledoc """
+  Documentation for `Weedx`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Weedx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/weedx_test.exs
@@ -0,0 +1,8 @@
+defmodule WeedxTest do
+  use ExUnit.Case
+  doctest Weedx
+
+  test "greets the world" do
+    assert Weedx.hello() == :world
+  end
+end
