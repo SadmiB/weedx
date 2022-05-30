e96e58970a4b5e507c75ad6b0c742fd47c40b036
Sadmi Bouhafs <sadmibouhafs@gmail.com>
2022-05-30 09:25:50 +0100

    Add delete entry (#28)
    * Add delete entry
    
    * apply format
    
    * update delete operation
    
    * apply format
    
    * update delete operation

--- lib/weedx/operation.ex
+++ lib/weedx/operation.ex
@@ -3,11 +3,17 @@ defmodule Weedx.Operation do
   A helper module to construct requests to feed to `Weedx` functions.
   """
 
-  alias Weedx.Filer.{ListEntriesRequest, AtomicRenameEntryRequest}
+  alias Weedx.Filer.{ListEntriesRequest, AtomicRenameEntryRequest, DeleteEntryRequest}
 
   @type list_entries_opts :: [
           {:limit, non_neg_integer()}
           | {:last_file_name, String.t()}
+        ]
+
+  @type delete_entry_opts :: [
+          {:recursive, boolean()}
+          | {:ignore_recursive_error, boolean()}
+          | {:from_other_cluster, boolean()}
         ]
 
   @doc """
@@ -32,8 +38,10 @@ defmodule Weedx.Operation do
 
   @spec move(String.t(), String.t()) :: AtomicRenameEntryResponse.t()
   def move(old_path, new_path) do
+    old_path = Path.absname(old_path)
     old_dir = Path.dirname(old_path)
     old_name = Path.basename(old_path)
+    new_path = Path.absname(new_path)
     new_dir = Path.dirname(new_path)
     new_name = Path.basename(new_path)
 
@@ -42,6 +50,30 @@ defmodule Weedx.Operation do
       old_name: old_name,
       new_directory: new_dir,
       new_name: new_name
+    })
+  end
+
+  @spec delete(String.t(), boolean(), delete_entry_opts()) :: DeleteEntryRequest.t()
+  def delete(
+        path,
+        delete_data,
+        opts \\ []
+      ) do
+    path = Path.absname(path)
+    dir = Path.dirname(path)
+    name = Path.basename(path)
+
+    recursive = Keyword.get(opts, :recursive, false)
+    ignore_recursive_error = Keyword.get(opts, :ignore_recursive_error, false)
+    from_other_cluster = Keyword.get(opts, :from_other_cluster, false)
+
+    DeleteEntryRequest.new!(%{
+      directory: dir,
+      name: name,
+      is_delete_data: delete_data,
+      is_recursive: recursive,
+      ignore_recursive_error: ignore_recursive_error,
+      is_from_other_cluster: from_other_cluster
     })
   end
 end
